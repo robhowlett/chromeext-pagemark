d491a97fecd88436793cfaf23f49787d0571d85e
Rob Howlett <rob@robhowlett.co.uk>
2017-04-18 07:04:35 +0100

    Allow for multiple tabs

--- background.js
+++ background.js
@@ -2,12 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var markSet = true;
+var markSet = {};
 var currentTab = 0;
 
-function updateIcon() {
-  markSet = !markSet;
-  if (markSet){
+function toggleValue(tabId) {
+  markSet[tabId] = !markSet[tabId];
+}
+
+function updateIcon(tabId) {
+
+  if(!(tabId in markSet)){
+    markSet[tabId] = false;
+  }
+  
+  if (markSet[tabId]){
       chrome.browserAction.setIcon({path:"icon2.png"});
       chrome.browserAction.setBadgeText({text: "set"});
   }else{
@@ -17,26 +25,26 @@ function updateIcon() {
 }
 
 //chrome.browserAction.setPopup({popup:"popup.html"});
-chrome.browserAction.onClicked.addListener(function() {
-  updateIcon();
+chrome.browserAction.onClicked.addListener(function(tab) {
+  toggleValue(tab.id);
+  updateIcon(tab.id);
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
       console.log(response.farewell);
     });
   });
 });
-updateIcon();
 
 chrome.tabs.onActivated.addListener(function(activeInfo){
-    alert(activeInfo.tabId);
+  updateIcon(activeInfo.tabId);
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId){
-    alert(tabId);
+  delete markSet[tabId];
 });
 
 chrome.tabs.query(
   { currentWindow: true, active: true },
   function (tabArray) {
-    alert(tabArray[0].id);
+    updateIcon(tabArray[0].id);
 });
