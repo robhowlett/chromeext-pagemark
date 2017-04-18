b35ac7a757866cd43081f34bbec4895f3d1a6f05
Rob Howlett <rob@robhowlett.co.uk>
2017-04-18 22:37:57 +0100

    Add visible marker to page

--- scroll-mark.js
+++ scroll-mark.js
@@ -9,12 +9,28 @@ var scrollPos = 0;
     function(request, sender, sendResponse) {
         if(request.action == "mark"){
             scrollPos = $(document).scrollTop();
+
+            var div = document.createElement("div");
+            div.style.width = "100%";
+            div.style.height = "5px";
+            div.style.background = "red";
+            div.style.position = "absolute";
+            div.style.top = scrollPos + "px";
+            div.style.zIndex = "1000";
+            div.style.opacity = "0.5";
+            div.id = "markerDiv";
+            document.body.appendChild(div);
+
             sendResponse({result: true});
         }else if(request.action == "jump"){
             $(document).scrollTop(scrollPos);
             sendResponse({result: true});
         }else if(request.action == "clear"){
             scrollPos = 0;
+
+            var markerDiv = document.getElementById('markerDiv');
+            markerDiv.parentNode.removeChild(markerDiv);
+
             sendResponse({result: true});
         }
     });
--- todo.md
+++ todo.md
@@ -1,3 +1,2 @@
 Breaks if you follow link/change pages
-Add marker to pages
 Make html only show relevant options
