2203118669a12755de4510c74c63356f80baaaa0
Rob Howlett <rob@robhowlett.co.uk>
2017-04-18 07:09:06 +0100

    Tidy formatting

--- background.js
+++ background.js
@@ -6,45 +6,43 @@ var markSet = {};
 var currentTab = 0;
 
 function toggleValue(tabId) {
-  markSet[tabId] = !markSet[tabId];
+    markSet[tabId] = !markSet[tabId];
 }
 
 function updateIcon(tabId) {
 
-  if(!(tabId in markSet)){
-    markSet[tabId] = false;
-  }
-  
-  if (markSet[tabId]){
-      chrome.browserAction.setIcon({path:"icon2.png"});
-      chrome.browserAction.setBadgeText({text: "set"});
-  }else{
-      chrome.browserAction.setIcon({path:"icon1.png"});
-      chrome.browserAction.setBadgeText({text: "none"});
-  }
+    if(!(tabId in markSet)){
+        markSet[tabId] = false;
+    }
+
+    if (markSet[tabId]){
+        chrome.browserAction.setIcon({path:"icon2.png"});
+        chrome.browserAction.setBadgeText({text: "set"});
+    }else{
+        chrome.browserAction.setIcon({path:"icon1.png"});
+        chrome.browserAction.setBadgeText({text: "none"});
+    }
 }
 
 //chrome.browserAction.setPopup({popup:"popup.html"});
 chrome.browserAction.onClicked.addListener(function(tab) {
-  toggleValue(tab.id);
-  updateIcon(tab.id);
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-      console.log(response.farewell);
+    toggleValue(tab.id);
+    updateIcon(tab.id);
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+            console.log(response.farewell);
+        });
     });
-  });
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo){
-  updateIcon(activeInfo.tabId);
+    updateIcon(activeInfo.tabId);
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId){
-  delete markSet[tabId];
+    delete markSet[tabId];
 });
 
-chrome.tabs.query(
-  { currentWindow: true, active: true },
-  function (tabArray) {
+chrome.tabs.query({currentWindow: true, active: true }, function (tabArray) {
     updateIcon(tabArray[0].id);
 });
--- scroll-mark.js
+++ scroll-mark.js
@@ -5,16 +5,15 @@
 var scrollPos = 0;
 
 (function ($) {
-//     $(document).scrollTop(300);
     chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      if(scrollPos === 0){
-        scrollPos = $(document).scrollTop();
-      }else{
-        $(document).scrollTop(scrollPos);
-        scrollPos = 0;
-      }
-      if (request.greeting == "hello")
-        sendResponse({farewell: "goodbye"});
+        if(scrollPos === 0){
+            scrollPos = $(document).scrollTop();
+        }else{
+            $(document).scrollTop(scrollPos);
+            scrollPos = 0;
+        }
+        if (request.greeting == "hello")
+            sendResponse({farewell: "goodbye"});
     });
 })(jQuery);
